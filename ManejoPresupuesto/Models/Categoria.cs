e70c5633a5a4395da8434182f9c4bd8340558c76
Andrés Correa <corrgom@gmail.com>
2025-06-12 21:29:37 -0500

    Se crearon los archivos y la lógica relacionada con las transacciones: Creación, actualización, vistas, servicios controlador, y borrado. También se centralizó el código JQuery en un archivo común, para mayor orden y centralización de código.

--- ManejoPresupuesto/Models/Categoria.cs
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -8,7 +8,7 @@ namespace ManejoPresupuesto.Models
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 50, ErrorMessage = "No puede ser mayor a {1} caractéres")]
         public string Nombre { get; set; }
-        [Display(Name = "Tipo Operación")]
+        [Display(Name = "Tipo de operación")]
         public TipoOperacion TipoOperacionId { get; set; }
         public int UsuarioId { get; set; }
     }
