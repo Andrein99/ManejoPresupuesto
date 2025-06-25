d0c3c7ba8d648b125c5ea97504355095367a3a24
Andrés Correa <corrgom@gmail.com>
2025-06-24 19:14:09 -0500

    Se creó la funcionalidad de movimientos de cuentas. Se refactorizó código que se utilizaba en diferentes archivos, y se centralizó código para evitar la redundancia. Se crean vistas para los diferentes reportes, y se focaliza el desarrollo del reporte diario, el submenú, y el reporte semanal.

--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -9,7 +9,10 @@ namespace ManejoPresupuesto.Servicios
         Task Actualizar(Transaccion transaccion, decimal montoAnterior, int cuentaAnterior);
         Task Borrar(int id);
         Task Crear(Transaccion transaccion);
+        Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(ObtenerTransaccionesPorCuenta modelo);
         Task<Transaccion> ObtenerPorId(int id, int usuarioId);
+        Task<IEnumerable<ResultadoObtenerPorSemana>> ObtenerPorSemana(ParametroObtenerTransaccionesPorUsuario modelo);
+        Task<IEnumerable<Transaccion>> ObtenerPorUsuarioId(ParametroObtenerTransaccionesPorUsuario modelo);
     }
     public class RepositorioTransacciones: IRepositorioTransacciones
     {
@@ -35,6 +38,41 @@ namespace ManejoPresupuesto.Servicios
                 commandType: System.Data.CommandType.StoredProcedure);
 
             transaccion.Id = id;
+        }
+
+        public async Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(
+            ObtenerTransaccionesPorCuenta modelo)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<Transaccion>(@"
+                SELECT t.Id, t.Monto, t.FechaTransaccion, c.Nombre as Categoria,
+                cu.Nombre as Cuenta, c.TipoOperacionId
+                FROM Transacciones t
+                INNER JOIN Categorias c
+                ON c.Id = t.CategoriaId
+                INNER JOIN Cuentas cu
+                ON cu.Id = t.CuentaId
+                WHERE t.CuentaId = @CuentaId AND t.UsuarioId = @UsuarioId
+                AND FechaTransaccion BETWEEN @FechaInicio AND @FechaFin",
+                modelo);
+        }
+
+        public async Task<IEnumerable<Transaccion>> ObtenerPorUsuarioId(
+            ParametroObtenerTransaccionesPorUsuario modelo)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<Transaccion>(@"
+                SELECT t.Id, t.Monto, t.FechaTransaccion, c.Nombre as Categoria,
+                cu.Nombre as Cuenta, c.TipoOperacionId
+                FROM Transacciones t
+                INNER JOIN Categorias c
+                ON c.Id = t.CategoriaId
+                INNER JOIN Cuentas cu
+                ON cu.Id = t.CuentaId
+                WHERE t.UsuarioId = @UsuarioId
+                AND FechaTransaccion BETWEEN @FechaInicio AND @FechaFin
+                ORDER BY t.FechaTransaccion DESC",
+                modelo);
         }
 
         public async Task Actualizar(Transaccion transaccion, decimal montoAnterior, 
@@ -68,6 +106,22 @@ namespace ManejoPresupuesto.Servicios
                   ON cat.Id = Transacciones.CategoriaId
                   WHERE Transacciones.Id = @Id AND Transacciones.UsuarioId = @UsuarioId",
                 new { Id = id, UsuarioId = usuarioId });
+        }
+
+        public async Task<IEnumerable<ResultadoObtenerPorSemana>> ObtenerPorSemana(
+            ParametroObtenerTransaccionesPorUsuario modelo)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<ResultadoObtenerPorSemana>(@"
+                    SELECT datediff(d, @fechaInicio, FechaTransaccion) / 7 + 1 as Semana,
+                    SUM(Monto) as Monto, cat.TipoOperacionId
+                    FROM Transacciones
+                    INNER JOIN Categorias cat
+                    ON cat.Id = Transacciones.CategoriaId
+                    WHERE Transacciones.UsuarioId = @usuarioId AND
+                    FechaTransaccion BETWEEN @fechaInicio AND @fechaFin
+                    GROUP BY datediff(d, @fechaInicio, FechaTransaccion) / 7, cat.TipoOperacionId",
+                    modelo);
         }
 
         public async Task Borrar(int id)
